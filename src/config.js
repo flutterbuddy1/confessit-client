b0ccdd8275bd9f4b124836dc59c29037861c05a4
Mayank Diwakar <78422111+flutterbuddy1@users.noreply.github.com>
2025-11-15 14:07:00 +0530

    Change apiUrl to use SERVER_ADDRESS

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
     appName: "ConfessIt",
-    apiUrl: import.meta.env.VITE_API_URL || "https://69.30.254.58:8080/api",
+    apiUrl: import.meta.env.VITE_API_URL || "SERVER_ADDRESS",
 
 }
